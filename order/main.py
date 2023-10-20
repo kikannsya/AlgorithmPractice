95db6fdf9827b0f28facf1f29cb37588c5af7465
kikannsya <66856613+kikannsya@users.noreply.github.com>
2023-10-20 21:59:46 +0900

    計算量の意味を確認

--- order/main.py
+++ order/main.py
@@ -1,8 +1,26 @@
+# O(log n)
 def func2(n):
     if n <= 1:
         return
     else:
         print(n)
         func2(n/2)
+#O(n)
+def func3(numbers):
+    for num in numbers:
+        print(num)
+#O(n * log n)
+def  func4(n):
+    for i in range(int(n)):
+        print(i , end=' ')
+    print()
 
-func (2)
+    if n <= 1:
+        return
+    func4(n/2)
+#O(n^2)
+def func5(numbers):
+    for i in range(len(numbres)):
+        for j in range(len(numbers)):
+            print(numbers[i], numbers[j])
+        print()
