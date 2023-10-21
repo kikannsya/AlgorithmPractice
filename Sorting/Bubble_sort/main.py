48064b59df707b5befa1079b889b29867ada4ad9
kikannsya <66856613+kikannsya@users.noreply.github.com>
2023-10-22 05:08:14 +0900

    Bubble最終

--- Sorting/Bubble_sort/main.py
+++ Sorting/Bubble_sort/main.py
@@ -1,13 +1,20 @@
+from typing import List
 
+#O(n^2)
+def bubble_sort(numbers: List[int]) -> List[int]:
+    len_numbers = len(numbers)
 
-def bubble_sort(numbers):
-    for i in range(len(numbers)-1):
-        for j in range(len(numbers)-i):
+    for i in range(len_numbers):
+        for j in range(len_numbers - 1 - i):
             if(numbers[j] > numbers[j + 1]):
-                buff = numbers[j]
-                numbers[j] = numbers[j+1]
-                numbers[j+1] = buff
+                numbers[j], numbers[j+1] = numbers[j+1], numbers[j]
+                #修正前：　上のようにすると良い
+                # buff = numbers[j]
+                # numbers[j] = numbers[j+1]
+                # numbers[j+1] = buff
     return numbers
 
 if "__name__" == "__main__":
-    print(bubble_sort([5, 2, 1, 3 , 4]))
+    import random
+    nums = [random.randint(0,1000) for i in range(19)]
+    print(bubble_sort(nums))
