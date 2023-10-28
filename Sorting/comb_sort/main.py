b3443b3ec9ecba3f8c9fe3db7d7166fc99303664
kikannsya <66856613+kikannsya@users.noreply.github.com>
2023-10-28 11:28:14 +0900

    comb_sort 書きかけ

---
+++ Sorting/comb_sort/main.py
@@ -0,0 +1,10 @@
+from typing import List
+
+def comb_sort(numbers: List[int]) -> List[int]:
+    len_numbers = len(numbers)
+    gap = len_numbers
+    swapped = True
+
+    while gap != 1:
+
+if __name__ == '__main__':
