8bac1ab22266eee585a839f97a6fe1e8934a1456
kikannsya <kuromaguro.shiki@gmail.com>
2023-12-19 23:34:11 +0900

    Bubble sort comment added

--- Sorting/Bubble_sort/main.py
+++ Sorting/Bubble_sort/main.py
@@ -3,9 +3,32 @@ from typing import List
 #O(n^2)
 def bubble_sort(numbers: List[int]) -> List[int]:
     len_numbers = len(numbers)
+## bubble sort
+#searching next to number
+# [1, 20, 10, 5]
+# [1, 20, 10, 5]
+# [1, 10, 20, 5]
+# [1, 10, 5, 20]    
+# [1, 5, 10, 20]
+#  for i in range(10):
+#      for j in range(10- 1 - i):   
+    #      print(j,end=' ')
+    #   print()
+#
+# 0 1 2 3 4 5 6 7 8 
+# 0 1 2 3 4 5 6 7 
+# 0 1 2 3 4 5 6
+# 0 1 2 3 4 5
+# 0 1 2 3 4
+# 0 1 2 3
+# 0 1 2
+# 0 1
+# 0
 
     for i in range(len_numbers):
         for j in range(len_numbers - 1 - i):
+            #First you check the number of numbers[i]
+            #Counts up to   
             if(numbers[j] > numbers[j + 1]):
                 numbers[j], numbers[j+1] = numbers[j+1], numbers[j]
                 #修正前：　上のようにすると良い
